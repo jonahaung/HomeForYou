05fd9d05034e28ae4cb62fe9b9bce796abba2759
jonahaung2 <aung_ko_min@transitlink.com.sg>
2024-05-25 15:35:38 +0800

    Initial Commit

---
+++ HomeForYou/HomeForYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeForYouApp.swift
+//  HomeForYou
+//
+//  Created by Aung Ko Min on 25/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeForYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
